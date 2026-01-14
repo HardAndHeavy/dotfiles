535fedcbf5e434a3c7d920b705fee7d548bd99ec
HardAndHeavy <HardAndHeavyMetal@yandex.ru>
2026-01-14 22:48:30 +0300

    добавлен аудиоредактор

--- install_for_me.sh
+++ install_for_me.sh
@@ -2,6 +2,7 @@ sudo apt-get install -y keepass2
 sudo apt-get install -y htop
 sudo apt-get install -y vlc # Видеоплеер
 sudo apt-get install -y musescore # Нотная программа
+sudo apt-get install -y ardour # Аудиоредактор
 
 # Телеграм
 curl -Lo tsetup.tar.xz https://telegram.org/dl/desktop/linux
