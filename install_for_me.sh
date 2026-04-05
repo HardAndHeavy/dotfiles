8c65d60594333412e9f780f6a96569b2770d6fa3
HardAndHeavy <HardAndHeavyMetal@yandex.ru>
2026-04-05 18:47:23 +0300

    update install telegram

--- install_for_me.sh
+++ install_for_me.sh
@@ -4,13 +4,7 @@ sudo apt-get install -y vlc # Видеоплеер
 sudo apt-get install -y musescore # Нотная программа
 sudo apt-get install -y ardour # Аудиоредактор
 sudo snap install blender --channel=5.1/stable --classic # Видеоредактор
-
-# Телеграм
-curl -Lo tsetup.tar.xz https://telegram.org/dl/desktop/linux
-sudo tar -xpf tsetup.tar.xz -C /opt
-sudo ln -s /opt/Telegram/Telegram /usr/local/bin/telegram-desktop
-sudo chmod -R 755 /opt/Telegram
-rm tsetup.tar.xz
+sudo snap install telegram
 
 # Макс
 MAX_GPG="/etc/apt/keyrings/max.gpg"
