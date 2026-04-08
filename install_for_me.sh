83aa724a79251bbd21ed6deb26f0a5b1bbbd3480
HardAndHeavy <HardAndHeavyMetal@yandex.ru>
2026-04-08 21:22:17 +0300

    add krita

--- install_for_me.sh
+++ install_for_me.sh
@@ -3,5 +3,6 @@ sudo apt-get install -y htop
 sudo apt-get install -y vlc # Видеоплеер
 sudo apt-get install -y musescore # Нотная программа
 sudo apt-get install -y ardour # Аудиоредактор
+sudo apt-get install -y krita # Графический редактор
 sudo snap install blender --channel=5.1/stable --classic # Видеоредактор
 sudo snap install telegram-desktop
