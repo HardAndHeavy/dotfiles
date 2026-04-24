f08277e0ae40e7f6efcf3305e63a30d7ba74ca46
HardAndHeavy <HardAndHeavyMetal@yandex.ru>
2026-04-24 19:58:27 +0300

    add yandex

--- install_for_me.sh
+++ install_for_me.sh
@@ -3,9 +3,25 @@ sudo apt install -y \
   htop \
   vlc \
   ardour \
-  krita \
-  https://repo.yandex.ru/yandex-browser/yandex-browser-stable_amd64.deb \
-  https://repo.yandex.ru/yandex-disk/yandex-disk_latest_amd64.deb
+  krita
 sudo snap install \
   blender \
   telegram-desktop
+
+# Yandex Browser
+sudo tee /etc/apt/sources.list.d/yandex-browser.sources > /dev/null <<EOF
+Types: deb
+URIs: https://repo.yandex.ru/yandex-browser/deb/
+Suites: stable
+Components: main
+Signed-By: /etc/apt/trusted.gpg.d/yandex-browser.gpg
+EOF
+sudo curl -fsSL https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG -o /etc/apt/trusted.gpg.d/yandex-browser.gpg
+sudo apt update
+sudo apt install -y yandex-browser-stable
+
+# Yandex Disk
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/yandex-disk.gpg] http://repo.yandex.ru/yandex-disk/deb/ stable main" | sudo tee /etc/apt/sources.list.d/yandex-disk.list > /dev/null
+sudo curl -fsSL https://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -o /etc/apt/trusted.gpg.d/yandex-disk.gpg
+sudo apt update
+sudo apt install -y yandex-disk
