f9a5a4ce93328430d11f8aeb5b97a5b0319844ba
HardAndHeavy <HardAndHeavyMetal@yandex.ru>
2026-04-24 20:05:19 +0300

    add yandex

--- install_for_me.sh
+++ install_for_me.sh
@@ -8,20 +8,25 @@ sudo snap install \
   blender \
   telegram-desktop
 
+sudo mkdir -p /usr/share/keyrings
+
 # Yandex Browser
-sudo tee /etc/apt/sources.list.d/yandex-browser.sources > /dev/null <<EOF
-Types: deb
-URIs: https://repo.yandex.ru/yandex-browser/deb/
-Suites: stable
-Components: main
-Signed-By: /etc/apt/trusted.gpg.d/yandex-browser.gpg
-EOF
-sudo curl -fsSL https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG -o /etc/apt/trusted.gpg.d/yandex-browser.gpg
+printf '%s\n' \
+  'Types: deb' \
+  'URIs: https://repo.yandex.ru/yandex-browser/deb/' \
+  'Suites: stable' \
+  'Components: main' \
+  'Signed-By: /usr/share/keyrings/yandex-browser.gpg' | \
+  sudo tee /etc/apt/sources.list.d/yandex-browser.sources > /dev/null
+curl -fsSL https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG | \
+  sudo gpg --dearmor --yes -o /usr/share/keyrings/yandex-browser.gpg
 sudo apt update
 sudo apt install -y yandex-browser-stable
 
 # Yandex Disk
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/yandex-disk.gpg] http://repo.yandex.ru/yandex-disk/deb/ stable main" | sudo tee /etc/apt/sources.list.d/yandex-disk.list > /dev/null
-sudo curl -fsSL https://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -o /etc/apt/trusted.gpg.d/yandex-disk.gpg
+echo "deb [signed-by=/usr/share/keyrings/yandex-disk.gpg] http://repo.yandex.ru/yandex-disk/deb/ stable main" | \
+  sudo tee /etc/apt/sources.list.d/yandex-disk.list > /dev/null
+curl -fsSL https://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG | \
+  sudo gpg --dearmor --yes -o /usr/share/keyrings/yandex-disk.gpg
 sudo apt update
 sudo apt install -y yandex-disk
